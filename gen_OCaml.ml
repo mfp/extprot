97b51f463f7b6a32f703618fa6453c19902b8750
Mauricio Fernandez <mfp@acm.org>
2008-10-07 12:30:43 +0200

    Fixes in Gen_OCaml.generate_container: don't use <:ctyp< >>.

--- gen_OCaml.ml
+++ gen_OCaml.ml
@@ -13,6 +13,15 @@ type container = {
 
 let (|>) x f = f x
 
+let foldl1 msg f g = function
+    [] -> invalid_arg ("foldl1: empty list -- " ^ msg)
+  | hd::tl -> List.fold_left g (f hd) tl
+
+let foldr1 msg f g = function
+    [] -> invalid_arg ("foldr1: empty list -- " ^ msg)
+  | l -> match List.rev l with
+        [] -> assert false
+      | hd::tl -> List.fold_left (fun s x -> g x s) (f hd) tl
 
 let generate_container bindings =
   let _loc = Loc.mk "gen_OCaml" in
@@ -25,36 +34,29 @@ let generate_container bindings =
 
   let rec message_types msgname = function
       `Record l ->
-      (* let fst_lbl, rest = match l with *)
-          (* hd::tl -> hd, tl *)
-        (* | _ -> failwith "generate_container: expected at least one field" in *)
-      let fields =
-        List.fold_left
-          (fun ctyp (name, mutabl, ty) ->
-             let tyexpr = ctyp_of_texpr ty in
-               if mutabl then
-                 <:ctyp< $ctyp$; $lid:name$ : mutable $tyexpr$ >>
-               else
-                 <:ctyp< $ctyp$; $lid:name$ : $tyexpr$ >>)
-          <:ctyp< >>
-          l
-      (* no quotations for type, wtf? *)
-      (* in <:str_item< type $msgname$ = { $fields$ } >> *)
-      in typedef msgname <:ctyp< { $fields$ } >>
+        let ctyp (name, mutabl, texpr) =
+          let ty = ctyp_of_texpr texpr in match mutabl with
+              true -> <:ctyp< $lid:name$ : mutable $ty$ >>
+            | false -> <:ctyp< $lid:name$ : $ty$ >> in
+        let fields =
+          foldl1 "message_types `Record" ctyp
+            (fun ct field -> <:ctyp< $ct$; $ctyp field$ >>) l
+        (* no quotations for type, wtf? *)
+        (* in <:str_item< type $msgname$ = { $fields$ } >> *)
+        in typedef msgname <:ctyp< { $fields$ } >>
 
    | `Sum l ->
+       let tydef_of_msg_branch (const, mexpr) =
+         message_types (msgname ^ "_" ^ const) (mexpr :> message_expr) in
        let record_types =
-         List.fold_left
-           (fun str_item (const, mexpr) ->
-              let tydef = message_types (msgname ^ "_" ^ const) (mexpr :> message_expr) in
-                <:str_item< $str_item$; $tydef$ >>)
-           <:str_item< >>
-           l in
-       let consts =
-         List.fold_left
-           (fun cs (const, _) -> <:ctyp< $cs$ | $uid:const$ of ($lid:msgname ^ "_" ^ const$) >>)
-           <:ctyp< >>
-           l
+         foldl1 "message_types `Sum" tydef_of_msg_branch
+           (fun s b -> <:str_item< $s$; $tydef_of_msg_branch b$ >>) l in
+
+       let variant (const, _) =
+         <:ctyp< $uid:const$ of ($lid: msgname ^ "_" ^ const $) >> in
+       let consts = foldl1 "message_types `Sum" variant
+                      (fun vars c -> <:ctyp< $vars$ | $variant c$ >>) l
+
        in <:str_item< $record_types$; $typedef msgname <:ctyp< [$consts$] >>$ >>
 
   and ctyp_of_texpr expr =
@@ -70,13 +72,13 @@ let generate_container bindings =
     | `List ty -> <:ctyp< list $ctyp_of_poly_texpr_core ty$ >>
     | `Array ty -> <:ctyp< array $ctyp_of_poly_texpr_core ty$ >>
     | `Tuple l ->
-        List.fold_right
+        foldr1 "ctyp_of_poly_texpr_core `Tuple" ctyp_of_poly_texpr_core
           (fun ptexpr tup -> <:ctyp< ( $ ctyp_of_poly_texpr_core ptexpr $ * $tup$ ) >>)
-          l <:ctyp< >>
+          l
     | `Type (name, args) ->
         let t = List.fold_left (* apply *)
                   (fun ty ptexpr -> <:ctyp< $ty$ $ctyp_of_poly_texpr_core ptexpr$ >>)
-                  <:ctyp< >>
+                  <:ctyp< $lid:name$ >>
                   args
         in (try <:ctyp< $id:Ast.ident_of_ctyp t$ >> with Invalid_argument _ -> t)
     | `Type_arg n -> <:ctyp< '$n$ >>
@@ -99,23 +101,33 @@ let generate_container bindings =
           } in
         let bogus_decls =
           List.map
-            (fun name -> (name, Type_decl ("<bogus>", [], `Sum (bogus_sum name))))
+            (fun name -> (name, Type_decl ("bogus", [], `Sum (bogus_sum name))))
             params in
         let bindings =
           update_bindings bindings
             (List.map fst bogus_decls) (List.map snd bogus_decls) in
         let ty = match poly_beta_reduce_texpr bindings texpr with
-            `Sum s ->
-              let const = List.fold_left
-                           (fun ctyp ty -> <:ctyp< $ctyp$ | $uid:ty$ >>)
-                           <:ctyp< >> s.constant
+            `Sum s -> begin
+              let ty_of_const_texprs (const, ptexprs) =
+                let ty = ctyp_of_poly_texpr_core (`Tuple ptexprs) in
+                  <:ctyp< $uid:const$ of $ty$ >>
 
-              in List.fold_left
-                   (fun ctyp (const, ptexprs) ->
-                      let ty = ctyp_of_poly_texpr_core (`Tuple ptexprs) in
-                        <:ctyp< $ctyp$ | $uid:const$ of $ty$ >>)
-                   const
-                   s.non_constant
+              in match s.constant with
+                  [] -> foldl1 "generate_container Type_decl `Sum"
+                          ty_of_const_texprs
+                          (fun ctyp c -> <:ctyp< $ctyp$ | $ty_of_const_texprs c$ >>)
+                          s.non_constant
+                | _ ->
+                    let const =
+                      foldl1 "generate_container `Sum"
+                        (fun tyn -> <:ctyp< $uid:tyn$ >>)
+                        (fun ctyp tyn -> <:ctyp< $ctyp$ | $uid:tyn$ >>)
+                        s.constant
+
+                    in List.fold_left
+                         (fun ctyp c -> <:ctyp< $ctyp$ | $ty_of_const_texprs c$ >>)
+                         const s.non_constant
+            end
           | #poly_type_expr_core ->
               reduce_to_poly_texpr_core bindings texpr |> ctyp_of_poly_texpr_core in
         let params = List.map (fun n -> <:ctyp< '$lid:n$ >>) params in
