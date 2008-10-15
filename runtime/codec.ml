158d97f011d580e47f88536606641fb5117ca382
Mauricio Fernandez <mfp@acm.org>
2008-10-15 17:45:36 +0200

    Extprot OCaml runtime, integrated in build system. Reworked Extprot.Error.

---
+++ runtime/codec.ml
@@ -0,0 +1,125 @@
+
+type low_level_type =
+    Vint
+  | Bits32
+  | Bits64
+  | Bytes
+  | Tuple
+  | Htuple
+
+type prefix = int
+
+let ll_type prefix = match prefix land 0xf with
+  (* varlen = 0 *)
+    0 (* ctyp 0 *) -> Vint
+  | 2 (* ctyp 1 *) -> Bits32
+  | 4 (* ctyp 2 *) -> Bits64
+  (* varlen = 1 *)
+  | 1 (* ctyp 0 *) -> Tuple
+  | 3 (* ctyp 1 *) -> Bytes
+  | 5 (* ctyp 2 *) -> Htuple
+  | _ -> Error.bad_format ()
+
+let ll_tag prefix = prefix lsr 4
+
+module Reader =
+struct
+  type t = { buf : string; last : int; mutable pos : int }
+
+  let make s off len =
+    if off < 0 || len < 0 || off + len > String.length s then
+      invalid_arg "Codec.Reader.make";
+    { buf = s; pos = off; last = off + len }
+
+  let read_byte t =
+    if t.pos >= t.last then raise End_of_file;
+    let r = Char.code t.buf.[t.pos] in
+      t.pos <- t.pos + 1;
+      r
+
+  let read_bytes t buf off len =
+    if off < 0 || len < 0 || off + len < String.length buf then
+      invalid_arg "Codec.Reader.read_bytes";
+    if len > t.last - t.pos then raise End_of_file;
+    String.blit t.buf t.pos buf off len;
+    t.pos <- t.pos + len
+
+  let read_vint t =
+    let b = ref (read_byte t) in
+    let x = ref 0 in
+    let e = ref 0 in
+      while !b >= 128 do
+        x := !x + ((!b - 128) lsl !e);
+        e := !e + 7;
+        b := read_byte t
+      done;
+      !x + (!b lsl !e)
+
+  let read_prefix = read_vint
+
+  let check_prim_type ty t =
+    let p = read_prefix t in
+      if ll_tag p <> 0 || ll_type p <> ty then Error.bad_format ()
+
+  let read_bool t =
+    check_prim_type Vint t;
+    match read_vint t with
+        0 -> false
+      | _ -> true
+
+  let read_positive_int t =
+    check_prim_type Vint t;
+    read_vint t
+
+  let read_rel_int t =
+    check_prim_type Vint t;
+    let n = read_vint t in
+      if n land 1 = 0 then (n lsr 1) else ((-n) asr 1)
+
+  let (+!) = Int32.add
+  let (<!) = Int32.shift_left
+  let to_i32 = Int32.of_int
+
+  let (+!!) = Int64.add
+  let (<!!) = Int64.shift_left
+  let to_i64 = Int64.of_int
+
+  let read_i32 t =
+    check_prim_type Bits32 t;
+    let a = read_byte t in
+    let b = read_byte t in
+    let c = read_byte t in
+    let d = read_byte t in
+      to_i32 a +! (to_i32 b <! 8) +! (to_i32 c <! 16) +! (to_i32 d <! 24)
+
+  let read_i64_bits t =
+    let a = read_byte t in
+    let b = read_byte t in
+    let c = read_byte t in
+    let d = read_byte t in
+    let e = read_byte t in
+    let f = read_byte t in
+    let g = read_byte t in
+    let h = read_byte t in
+      to_i64 a          +!! (to_i64 b <!! 8) +!!
+      (to_i64 c <!! 16) +!! (to_i64 d <!! 24) +!!
+      (to_i64 e <!! 32) +!! (to_i64 f <!! 40) +!!
+      (to_i64 g <!! 48) +!! (to_i64 h <!! 56)
+
+  let read_i64 t =
+    check_prim_type Bits64 t;
+    read_i64_bits t
+
+  let read_float t =
+    check_prim_type Bits64 t;
+    Int64.float_of_bits (read_i64_bits t)
+
+  let read_string t =
+    let prefix = read_prefix t in
+      if ll_tag prefix <> 0 || ll_type prefix <> Bytes then
+        Error.bad_format ();
+      let len = read_vint t in
+      let s = String.create len in
+        read_bytes t s 0 len;
+        s
+end
--- runtime/error.ml
+++ runtime/error.ml
@@ -1,19 +1,28 @@
+open Printf
+
+type field_format_error =
+    Bad_field_format
+  | Bad_field_tag
+
+type message_error =
+    Bad_message_type
+  | Bad_message_tag
+
 type format_error =
-    Bad_field of string * string
-  | Bad_message_type
-  | Unknown_message_tag of int
-  | Unknown_field_tag of int
+    Field_error of string * string * string * field_format_error
+  | Message_error of string * message_error
+  | Unknown_error
 
 type extprot_error =
     Missing_element of string * string * string * int
   | Missing_field of string * string * string
-  | Bad_format of string * format_error
+  | Bad_format of format_error
 
 exception Extprot_error of extprot_error * string
 
-(* let extprot_error err fmt = kprintf (fun s -> raise (Extprot_error (err, s))) fmt *)
 let extprot_error err fmt =
-  kprintf (fun s -> raise (Extprot_error (err, s)); if true then assert false) fmt
+  (* kprintf (fun s -> if true then raise (Extprot_error (err, s))) fmt *)
+  kprintf (fun s -> raise (Extprot_error (err, s))) fmt
 
 let missing_element msg constr field elm =
   extprot_error (Missing_element (msg, constr, field, elm))
@@ -25,18 +34,23 @@ let missing_field msg constr field =
     "Missing %S field in message %S, constructor %S." field msg constr
 
 let bad_field_format msg constr field =
-  extprot_error (Bad_format (msg, (Bad_field (constr, field))))
+  extprot_error (Bad_format (Field_error (msg, constr, field, Bad_field_format)))
     "Bad field format for message %S, constructor %S, field %S."
     msg constr field
 
 let unknown_field_tag msg constr field tag =
-  extprot_error (Bad_format (msg, (Unknown_field_tag (constr, field, tag))))
+  extprot_error (Bad_format (Field_error (msg, constr, field, Bad_field_tag)))
     "Unknown tag %d for message %S, constructor %S, field %S."
     tag msg constr field
 
 let bad_message_type msg =
-  extprot_error (Bad_format (Bad_message_type ty)) "Bad message type"
+  extprot_error (Bad_format (Message_error (msg, Bad_message_type))) "Bad message type"
 
 let unknown_message_tag msg tag =
-  extprot_error (Bad_format (Unknown_message_tag tag))
+  extprot_error (Bad_format (Message_error (msg, Bad_message_tag)))
     "Bad message tag %d" tag
+
+let bad_format () =
+  extprot_error (Bad_format Unknown_error)
+    "Unexpected or unknown type"
+
