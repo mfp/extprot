ec801416c19099ed2603839aafef7c5ac181119d
Mauricio Fernandez <mfp@acm.org>
2008-10-19 18:30:02 +0200

    Pretty-printing of Extprot errors.

--- runtime/error.ml
+++ runtime/error.ml
@@ -1,4 +1,5 @@
 open Printf
+module PP = Pretty_print
 
 type field_format_error =
     Bad_field_format
@@ -19,6 +20,38 @@ type extprot_error =
   | Bad_format of format_error
 
 exception Extprot_error of extprot_error * string
+
+let pp_field_format_error pp = function
+    Bad_field_format -> PP.fprintf pp "Bad_field_format"
+  | Bad_field_tag -> PP.fprintf pp "Bad_field_tag"
+
+let pp_message_format_error pp = function
+  | Bad_message_type -> PP.fprintf pp "Bad_message_type"
+  | Bad_message_tag -> PP.fprintf pp "Bad_message_tag"
+
+let pp_format_error pp = function
+    Field_error (msg, constr, field, field_format_error) ->
+      PP.pp_tuple4 ~constr:"Field_error"
+        PP.pp_string PP.pp_string PP.pp_string pp_field_format_error
+        pp (msg, constr, field, field_format_error)
+  | Message_error (s, msg_error) ->
+      PP.pp_tuple2 ~constr:"Message_error"
+        PP.pp_string pp_message_format_error
+        pp (s, msg_error)
+  | Unknown_error -> PP.fprintf pp "Unknown_error"
+
+let pp_extprot_error pp = function
+    Missing_element (message, constr, field, elmno) ->
+      PP.pp_tuple4 ~constr:"Missing_element"
+        PP.pp_string PP.pp_string PP.pp_string PP.pp_int
+        pp (message, constr, field, elmno)
+  (* | Missing_field (message, constr, field) -> *)
+  | Missing_field (message, constr, field) ->
+      PP.pp_tuple3 ~constr:"Missing_field"
+        PP.pp_string PP.pp_string PP.pp_string
+        pp (message, constr, field)
+  | Bad_format err ->
+      PP.fprintf pp "Bad_format %a" pp_format_error err
 
 let extprot_error err fmt =
   (* kprintf (fun s -> if true then raise (Extprot_error (err, s))) fmt *)
