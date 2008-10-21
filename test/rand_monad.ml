cd9ee2ca2149700c06ee9af6e4722146810ffa30
Mauricio Fernandez <mfp@acm.org>
2008-10-21 20:02:53 +0200

    Added Rand_monad.Rand.rand_tupleN.

--- test/rand_monad.ml
+++ test/rand_monad.ml
@@ -25,6 +25,16 @@ sig
   val rand_list : int t -> 'a t -> 'a list t
   val rand_array : int t -> 'a t -> 'a array t
   val rand_choice : 'a t list -> 'a t
+  val rand_tuple2 : 'a t -> 'b t -> ('a * 'b) t
+  val rand_tuple3 : 'a t -> 'b t -> 'c t -> ('a * 'b * 'c) t
+  val rand_tuple4 : 'a t -> 'b t -> 'c t -> 'd t -> ('a * 'b * 'c * 'd) t
+  val rand_tuple5 :
+    'a t -> 'b t -> 'c t -> 'd t -> 'e t -> ('a * 'b * 'c * 'd  * 'e) t
+  val rand_tuple6 :
+    'a t -> 'b t -> 'c t -> 'd t -> 'e t -> 'f t -> ('a * 'b * 'c * 'd  * 'e * 'f) t
+  val rand_tuple7 :
+    'a t -> 'b t -> 'c t -> 'd t -> 'e t -> 'f t -> 'g t ->
+    ('a * 'b * 'c * 'd  * 'e * 'f * 'g) t
 end
 
 module Rand : sig
@@ -79,6 +89,28 @@ struct
   let rand_array len elm = rand_list len elm >>= fun l -> return (Array.of_list l)
 
   let rand_choice l = rand_integer (List.length l) >>= List.nth l
+
+  let rand_tuple2 a b =
+    a >>= fun a -> b >>= fun b -> return (a, b)
+
+  let rand_tuple3 a b c =
+    a >>= fun a -> b >>= fun b -> c >>= fun c -> return (a, b, c)
+
+  let rand_tuple4 a b c d =
+    a >>= fun a -> b >>= fun b -> c >>= fun c -> d >>= fun d -> return (a, b, c, d)
+
+  let rand_tuple5 a b c d e =
+    a >>= fun a -> b >>= fun b ->
+    c >>= fun c -> d >>= fun d -> e >>= fun e -> return (a, b, c, d, e)
+
+  let rand_tuple6 a b c d e f =
+    a >>= fun a -> b >>= fun b -> c >>= fun c ->
+    d >>= fun d -> e >>= fun e -> f >>= fun f -> return (a, b, c, d, e, f)
+
+  let rand_tuple7 a b c d e f g =
+    a >>= fun a -> b >>= fun b -> c >>= fun c ->
+    d >>= fun d -> e >>= fun e -> f >>= fun f -> g >>= fun g ->
+      return (a, b, c, d, e, f, g)
 
   let run ?(state = R.get_state ()) m = fst (m state)
 
