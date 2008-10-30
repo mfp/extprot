8c4f9ea18ccc9280a5d6c34f88ff2aa6688da5c1
Mauricio Fernandez <mfp@acm.org>
2008-10-30 19:52:18 +0100

    Changes in cmdline parsing of extprotc.

--- compiler/extprotc.ml
+++ compiler/extprotc.ml
@@ -3,38 +3,44 @@ open Printf
 open Extprot_gen
 open ExtList
 
-let file = match Array.length Sys.argv with
-    n when n > 1 -> Sys.argv.(1)
-  | _ -> "tst.proto"
-
-let decls = Parser.print_synerr Parser.parse_file file
-let bindings = Gencode.collect_bindings decls
-
 module G = Gencode.Make(Gen_OCaml)
-module PP = Gencode.Prettyprint
 
 let (|>) x f = f x
 
-let print_field const fname mutabl ty =
+let file = ref None
+
+let arg_spec =
+  Arg.align
+    [
+    ]
+
+let () =
+  Arg.parse arg_spec (fun fname -> file := Some fname) "Usage: extprotc <file>";
+  Option.may
+    (fun file ->
+       let decls = Parser.print_synerr Parser.parse_file file in
+       (* let bindings = Gencode.collect_bindings decls in *)
+         G.generate_code decls |> print_endline)
+    !file
+
+module PP = Gencode.Prettyprint
+
+let print_field bindings const fname mutabl ty =
   let reduced = Ptypes.type_expr ty |> Gencode.beta_reduce_texpr bindings in
     Format.fprintf Format.std_formatter "%!%S - %S mutable: %s\n @[<1>%a@]\n\n%!"
       const fname (string_of_bool mutabl) PP.pp_reduced_type_expr reduced
 
-let () =
-  G.generate_code decls |> print_endline;
-
-  if false then begin
-    print_newline ();
-    print_endline (String.make 60 '*');
-    print_newline ();
-    List.iter
-      (function
-           Ptypes.Message_decl (name, mexpr) ->
-             Format.fprintf Format.std_formatter "Message %S:\n" name;
-             Gencode.iter_message print_field mexpr;
-             Format.fprintf Format.std_formatter "\n---\n\n"
-         | Ptypes.Type_decl (_, _, _) -> ())
-      decls;
-  end
+let inspect_decls decls bindings =
+  print_newline ();
+  print_endline (String.make 60 '*');
+  print_newline ();
+  List.iter
+    (function
+         Ptypes.Message_decl (name, mexpr) ->
+           Format.fprintf Format.std_formatter "Message %S:\n" name;
+           Gencode.iter_message (print_field bindings) mexpr;
+           Format.fprintf Format.std_formatter "\n---\n\n"
+       | Ptypes.Type_decl (_, _, _) -> ())
+    decls;
 
 
