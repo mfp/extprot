9aae1e2bb0cf4469cbb4719f27bbda79b2761e0d
Mauricio Fernandez <mfp@acm.org>
2008-11-01 13:37:07 +0100

    Command-line parsing in bm_expat.

--- test/bm_expat.ml
+++ test/bm_expat.ml
@@ -11,14 +11,21 @@ let parse s =
       (Expat.xml_error_to_string e);
     raise exn
 
-let rounds = 10
+let rounds = ref 10
+
+let arg_spec =
+  Arg.align
+    [
+      "-n", Arg.Set_int rounds, "INT Number of iterations."
+    ]
 
 let () =
+  Arg.parse arg_spec ignore "\nUsage: bm_expat [OPTIONS]\n\nOptions:" in
   let s = String.concat "\n" ["<doc>"; Std.input_all stdin; "</doc>"] in
   let dts =
-    Array.init rounds
+    Array.init !rounds
       (fun _ -> let t0 = Unix.gettimeofday () in parse s; Unix.gettimeofday () -. t0)
   in
     printf "Read %d bytes.\n" (String.length s);
-    printf "Avg: %8.5fs\n" (Array.fold_left (+.) 0. dts /. float rounds);
+    printf "Avg: %8.5fs\n" (Array.fold_left (+.) 0. dts /. float !rounds);
     printf "Min: %8.5fs\n" (Array.fold_left min max_float dts)
