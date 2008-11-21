ae2d4b8903c5f654694776ef250212f5bd9b787a
Mauricio Fernandez <mfp@acm.org>
2008-11-21 12:29:54 +0100

    Added Extprot.Conv.read and write, for reading(writing) from(to) IOs.

--- runtime/conv.ml
+++ runtime/conv.ml
@@ -5,3 +5,13 @@ let serialize f x =
     Msg_buffer.contents b
 
 let deserialize f s = f (Reader.String_reader.from_string s)
+
+let read f io = f (Reader.IO_reader.from_io io)
+
+let write ?buf (f : Msg_buffer.t -> 'a -> unit) io (x : 'a) =
+  let buf = match buf with
+      None -> Msg_buffer.create ()
+    | Some b -> Msg_buffer.clear b; b
+  in
+    f buf x;
+    IO.nwrite io (Msg_buffer.contents buf)
