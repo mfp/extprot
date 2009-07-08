bcede20c6c1722a72228f3ca9dc4a39a3d5366a2
Mauricio Fernandez <mfp@acm.org>
2009-07-08 12:55:11 +0200

    Moved type defs in compiler to separate files.

--- compiler/gencode.ml
+++ compiler/gencode.ml
@@ -3,71 +3,9 @@ open Ptypes
 open ExtList
 open Printf
 
+include Gencode_types
+
 let (|>) x f = f x
-
-type tag = int
-
-type low_level =
-    Vint of vint_meaning * type_options
-  | Bitstring32 of type_options
-  | Bitstring64 of b64_meaning * type_options
-  | Bytes of type_options
-  | Sum of constructor list * (constructor * low_level list) list * type_options
-  | Record of string * field list * type_options
-  | Tuple of low_level list * type_options
-  | Htuple of htuple_meaning * low_level * type_options
-  | Message of string * type_options
-
-and constructor = {
-  const_tag : tag;
-  const_name : string;
-  const_type : string;
-}
-
-and field = {
-  field_name : string;
-  field_type : low_level;
-}
-
-and 'a message =
-  | Message_single of string option * (string * bool * 'a) list
-  | Message_sum of (string * (string * bool * 'a) list) list
-
-and vint_meaning =
-    Bool
-  | Int8
-  | Int
-
-and b64_meaning =
-    Long
-  | Float
-
-and htuple_meaning =
-    List
-  | Array
-
-type reduced_type_expr = [
-    reduced_type_expr base_type_expr_core
-  | `Sum of reduced_type_expr sum_data_type * type_options
-  | `Record of reduced_type_expr record_data_type * type_options
-  | `Message of string * type_options
-]
-
-type poly_type_expr_core = [
-    poly_type_expr_core base_type_expr_core
-  | `Type of string * poly_type_expr_core list * type_options (* polymorphic sum type name, type args *)
-  | `Type_arg of string
-]
-
-type poly_type_expr = [
-  poly_type_expr_core
-  | `Record of poly_type_expr_core record_data_type * type_options
-  | `Sum of poly_type_expr_core sum_data_type * type_options
-]
-
-let reduced_type_expr e = (e :> reduced_type_expr)
-
-type bindings = declaration SMap.t
 
 let failwithfmt fmt = kprintf (fun s -> if true then failwith s) fmt
 
---
+++ compiler/protocol_types.ml
@@ -0,0 +1,84 @@
+open Printf
+
+type type_options = (string * string) list
+
+let dump_type_options l =
+  let pr_list name l =
+    printf "%s options:\n" name;
+    List.iter (fun (k, v) -> printf "%s.%S = %S\n" name k v) l in
+  let dump_opt = function
+      `Global l -> pr_list "Global" l
+    | `OCaml l -> pr_list "OCaml" l
+  in List.iter dump_opt l
+
+type base_type_expr_simple = [
+    `Bool of type_options
+  | `Byte of type_options
+  | `Int of type_options
+  | `Long_int of type_options
+  | `Float of type_options
+  | `String of type_options
+]
+
+type 'a base_type_expr_core = [
+    base_type_expr_simple
+  | `Tuple of 'a list * type_options
+  | `List of 'a * type_options
+  | `Array of 'a * type_options
+]
+
+module Type_param : sig
+  type type_param
+  val type_param_of_string : string -> type_param
+  val string_of_type_param : type_param -> string
+  val type_param_name : type_param -> string
+end =
+struct
+  type type_param = string
+  let type_param_of_string x = x
+  let string_of_type_param x = "'" ^ x
+  let type_param_name x = x
+end
+
+include Type_param
+
+type base_type_expr = [
+    base_type_expr base_type_expr_core
+  | `App of string * base_type_expr list * type_options
+  | `Type_param of type_param
+]
+
+type type_expr = [
+    base_type_expr
+  | `Record of base_type_expr record_data_type * type_options
+  | `Sum of base_type_expr sum_data_type * type_options
+]
+
+and 'a record_data_type = {
+  record_name : string;
+  record_fields : (string * bool * 'a) list
+}
+
+and 'a sum_data_type = {
+  type_name : string;
+  constant : string list;
+  non_constant : (string * 'a list) list
+}
+
+type base_message_expr = [ `Record of (string * bool * base_type_expr) list ]
+
+
+type message_expr_app = [ `App of string * base_type_expr list * type_options ]
+
+type message_expr = [
+    base_message_expr
+  | message_expr_app
+  | `Sum of (string * [base_message_expr | message_expr_app]) list
+]
+
+type declaration =
+    Message_decl of string * message_expr * type_options
+  | Type_decl of string * type_param list * type_expr * type_options
+
+let base_type_expr e = (e :> base_type_expr)
+let type_expr e = (e :> type_expr)
