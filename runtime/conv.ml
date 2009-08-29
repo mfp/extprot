58c3e5b6f5c1f496a32e8190da3afdf987523df6
Mauricio Fernandez <mfp@acm.org>
2009-08-29 16:15:29 +0200

    Conv: new read_versioned and write_versioned functions, documented intf.

--- runtime/conv.ml
+++ runtime/conv.ml
@@ -1,4 +1,6 @@
 
+exception Wrong_protocol_version of int * int (* max_known, found *)
+
 let serialize f x =
   let b = Msg_buffer.create () in
     f b x;
@@ -8,10 +10,24 @@ let deserialize f s = f (Reader.String_reader.from_string s)
 
 let read f io = f (Reader.IO_reader.from_io io)
 
+let get_buf = function
+    None -> Msg_buffer.create ()
+  | Some b -> Msg_buffer.clear b; b
+
 let write ?buf (f : Msg_buffer.t -> 'a -> unit) io (x : 'a) =
-  let buf = match buf with
-      None -> Msg_buffer.create ()
-    | Some b -> Msg_buffer.clear b; b
-  in
+  let buf = get_buf buf in
     f buf x;
+    IO.nwrite io (Msg_buffer.contents buf)
+
+let read_versioned fs io =
+  let version = IO.read_ui16 io in
+    if version < Array.length fs then
+      fs.(version) io
+    else
+      raise (Wrong_protocol_version ((Array.length fs), version))
+
+let write_versioned ?buf version (f : Msg_buffer.t -> 'a -> unit) io (x : 'a) =
+  let buf = get_buf buf in
+    f buf x;
+    IO.write_ui16 io version;
     IO.nwrite io (Msg_buffer.contents buf)
---
+++ runtime/conv.mli
@@ -0,0 +1,31 @@
+(** Convenience functions to serialize and deserialize values to/from strings
+  * and extlib IO channels. *)
+
+(** [Wrong_protocol_version (max_known, found)] *)
+exception Wrong_protocol_version of int * int
+
+val serialize : (Msg_buffer.t -> 'a -> 'b) -> 'a -> string
+val deserialize : (Reader.String_reader.t -> 'a) -> string -> 'a
+
+val read : (Reader.IO_reader.t -> 'a) -> IO.input -> 'a
+
+(** @param buf the buffer to use when serializing the value. It will be
+  *            automatically cleared before use. *)
+val write :
+  ?buf:Msg_buffer.t ->
+  (Msg_buffer.t -> 'a -> unit) -> 'b IO.output -> 'a -> unit
+
+(* [read_versioned fs io] reads a little-endian, 16-bit, unsigned integer
+ * and uses it to index the [fs] array of reader functions.
+ * @raise Wrong_protocol_version if the version is higher than the last known
+ * one.
+ * *)
+val read_versioned : (IO.input -> 'a) array -> IO.input -> 'a
+
+(** [write_versioned ?buf version f io x] writes the given [version] as a
+  * 16-bit, unsigned integer to [io], followed by the serialization of the
+  * value as performed by [f]. If the serialization fails, nothing is written.
+  * *)
+val write_versioned :
+  ?buf:Msg_buffer.t ->
+  int -> (Msg_buffer.t -> 'a -> unit) -> 'b IO.output -> 'a -> unit
