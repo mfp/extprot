735e5bf2be648c483f84c92a961acd62c74ec4db
Mauricio Fernandez <mfp@acm.org>
2009-08-29 15:24:35 +0200

    Fix primite reader functions: skip whole value if wrong wire type found.

--- runtime/reader_impl.ml
+++ runtime/reader_impl.ml
@@ -21,7 +21,7 @@ DEFINE Read_prim_type_no_prefix(t, p, ty, read_fast, read_fallback) =
      skip_value t p;
      Error.unknown_tag (ll_tag p)
    end else if llty <> Tuple then
-     read_fallback t llty
+     read_fallback t p llty
    else begin (* read 1st element from tuple *)
      let len = read_vint t in
      let eot = offset t len in
@@ -39,7 +39,7 @@ DEFINE Read_prim_type_no_prefix(t, p, ty, read_fast, read_fallback) =
                    v
              | llty -> begin
                  try
-                   read_fallback t llty
+                   read_fallback t p llty
                  with Error.Extprot_error _ as e ->
                    skip_to t eot;
                    raise e
@@ -58,7 +58,9 @@ let read_raw_bool t = match read_byte t with
     0 -> false
   | _ -> true
 
-let nil_fallback t llty = Error.bad_wire_type ()
+let nil_fallback t p llty =
+  skip_value t p;
+  Error.bad_wire_type ()
 
 let read_bool t = Read_prim_type(t, Bits8, read_raw_bool, nil_fallback)
 
@@ -69,11 +71,11 @@ let read_i8 t = Read_prim_type(t, Bits8, read_raw_i8, nil_fallback)
 let read_raw_rel_int t =
   let n = read_vint t in (n lsr 1) lxor (- (n land 1))
 
-let read_rel_int_fallback t = function
+let read_rel_int_fallback t p = function
     Bits8 -> read_raw_i8 t
   | Vint -> read_raw_rel_int t
   | Bits32 | Bits64_long | Bits64_float
-  | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type -> Error.bad_wire_type ()
+  | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type as ty -> nil_fallback t p ty
 
 let read_rel_int t = Read_prim_type(t, Vint, read_raw_rel_int, read_rel_int_fallback)
 
@@ -92,12 +94,12 @@ let read_raw_i32 t =
   let d = read_byte t in
     to_i32 a +! (to_i32 b <! 8) +! (to_i32 c <! 16) +! (to_i32 d <! 24)
 
-let read_i32_fallback t = function
+let read_i32_fallback t p = function
     Bits8 -> Int32.of_int (read_raw_i8 t)
   | Vint -> Int32.of_int (read_raw_rel_int t)
   | Bits32 -> read_raw_i32 t
   | Bits64_long | Bits64_float
-  | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type -> Error.bad_wire_type ()
+  | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type as ty -> nil_fallback t p ty
 
 let read_i32 t = Read_prim_type(t, Bits32, read_raw_i32, read_i32_fallback)
 
@@ -117,13 +119,13 @@ let read_i64_bits t =
 
 let read_raw_i64 = read_i64_bits
 
-let read_i64_fallback t = function
+let read_i64_fallback t p = function
     Bits8 -> Int64.of_int (read_raw_i8 t)
   | Vint -> Int64.of_int (read_raw_rel_int t)
   | Bits32 -> Int64.of_int32 (read_raw_i32 t)
   | Bits64_long -> read_raw_i64 t
-  | Bits64_float | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type ->
-      Error.bad_wire_type ()
+  | Bits64_float | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type as ty ->
+      nil_fallback t p ty
 
 let read_i64 t = Read_prim_type(t, Bits64_long, read_raw_i64, read_i64_fallback)
 
@@ -146,13 +148,13 @@ let read_raw_float t =
     Int64.float_of_bits (read_i64_bits t)
   END
 
-let read_float_fallback t = function
+let read_float_fallback t p = function
     Bits8 -> float (read_raw_i8 t)
   | Vint -> float (read_raw_rel_int t)
   | Bits32 -> Int32.to_float (read_raw_i32 t)
   | Bits64_long -> Int64.to_float (read_raw_i64 t)
   | Bits64_float -> read_raw_float t
-  | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type -> Error.bad_wire_type ()
+  | Enum | Tuple | Bytes | Htuple | Assoc | Invalid_ll_type as ty -> nil_fallback t p ty
 
 let read_float t = Read_prim_type(t, Bits64_float, read_raw_float, read_float_fallback)
 
