5d11e4aba7e76208c06262b43c545bd1b5bb1585
Mauricio Fernandez <mfp@acm.org>
2011-06-07 16:22:50 +0200

    OCaml runtime: minor format tweaks in Pretty_print.

--- runtime/pretty_print.ml
+++ runtime/pretty_print.ml
@@ -25,19 +25,19 @@ let rec fprintfs sep f pp = function
 
 let pp_list f pp l =
   let pr_elems = fprintfs ";@ " f in
-    fprintf pp "[@[<1>@ %a@ @]]" pr_elems l
+    fprintf pp "[@[ %a @]]" pr_elems l
 
 let pp_array f pp l =
   let pr_elems = fprintfs ";@ " f in
-    fprintf pp "[|@[<1>@ %a@ @]|]" pr_elems (Array.to_list l)
+    fprintf pp "[|@[ %a @]|]" pr_elems (Array.to_list l)
 
 let pp_struct fields pp t =
   let rec pr_fields pp = function
       [] -> ()
-    | [name, f] -> fprintf pp "@[<1>%s@ =@ %a@]" name f t
-    | (name, f)::l -> fprintf pp "@[<1>%s@ =@ %a@];@ %a" name f t pr_fields l
+    | [name, f] -> fprintf pp "@[<2>%s@ =@ %a@]" name f t
+    | (name, f)::l -> fprintf pp "@[<2>%s@ =@ %a@];@ %a" name f t pr_fields l
   in
-    fprintf pp "{@[<1>@ %a @]}" pr_fields fields
+    fprintf pp "{ @[%a@] }" pr_fields fields
 
 let constr_string = function
     None -> ""
@@ -48,22 +48,22 @@ let pp_option f pp = function
   | Some x -> fprintf pp "Some %a" f x
 
 let pp_tuple2 ?constr f1 f2 pp (a, b) =
-  fprintf pp "%s(@[<1>%a,@ %a@])" (constr_string constr) f1 a f2 b
+  fprintf pp "%s(@[%a,@ %a@])" (constr_string constr) f1 a f2 b
 
 let pp_tuple3 ?constr f1 f2 f3 pp (a, b,c) =
-  fprintf pp "%s(@[<1>%a,@ %a,@ %a@])"
+  fprintf pp "%s(@[%a,@ %a,@ %a@])"
     (constr_string constr) f1 a f2 b f3 c
 
 let pp_tuple4 ?constr f1 f2 f3 f4 pp (a, b, c, d) =
-  fprintf pp "%s(@[<1>%a,@ %a,@ %a,@ %a@])"
+  fprintf pp "%s(@[%a,@ %a,@ %a,@ %a@])"
     (constr_string constr) f1 a f2 b f3 c f4 d
 
 let pp_tuple5 ?constr f1 f2 f3 f4 f5 pp (a, b, c, d, e) =
-  fprintf pp "%s(@[<1>%a,@ %a,@ %a,@ %a,@ %a@])"
+  fprintf pp "%s(@[%a,@ %a,@ %a,@ %a,@ %a@])"
     (constr_string constr) f1 a f2 b f3 c f4 d f5 e
 
 let pp_tuple6 ?constr f1 f2 f3 f4 f5 f6 pp (a, b, c, d, e, f) =
-  fprintf pp "%s(@[<1>%a,@ %a,@ %a,@ %a,@ %a,@ %a@])"
+  fprintf pp "%s(@[%a,@ %a,@ %a,@ %a,@ %a,@ %a@])"
     (constr_string constr) f1 a f2 b f3 c f4 d f5 e f6 f
 
 let pp_field fieldf f pp t = f pp (fieldf t)
@@ -89,7 +89,7 @@ let pp_int64 pp n =
     fprintf pp fmt (Int64.to_string n)
 
 let pp_bytes f pp s =
-  fprintf pp "Bytes [@[<2> %a@]@ ]"
+  fprintf pp "Bytes@ [@[%a@] ]"
     (fprintfs "@ " f)
     (List.map Char.code @@ String.explode s)
 
