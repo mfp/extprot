8f4ef87f9cbe1074645e609d5a37613cfea2671d
Mauricio Fernandez <mfp@acm.org>
2020-04-28 00:22:39 +0200

    Add runtime support for lazy fields: Extprot.Field.

---
+++ runtime/field.ml
@@ -0,0 +1,96 @@
+
+module type S =
+sig
+  type 'a t
+
+  val from_val : 'a -> 'a t
+  val from_fun :
+    Reader.String_reader.t ->
+    (Reader.String_reader.t -> 'a) -> 'a t
+
+
+  val is_val     : 'a t -> bool
+  val force      : 'a t -> 'a
+  val get_reader : 'a t -> Reader.String_reader.t option
+end
+
+module Fast_write =
+struct
+  type 'a t =
+    {
+      mutable s : Reader.String_reader.t option;
+      mutable v : 'a memo;
+    }
+
+  and 'a memo =
+    | Delayed of Reader.String_reader.t * (Reader.String_reader.t -> 'a)
+    | Value of 'a
+
+  let from_val v = { s = None; v = Value v }
+
+  let from_fun s f = { s = Some s; v = Delayed (s, f) }
+
+  let force t = match t.v with
+    | Value x -> x
+    | Delayed (s, f) ->
+        let x = f s in
+          t.v <- Value x;
+          x
+
+  let is_val t = match t.v with
+    | Value _ -> true
+    | Delayed _ -> false
+
+  let get_reader t = t.s
+end
+
+module Discard_data =
+struct
+  include Fast_write
+
+  let force t = match t.v with
+    | Value x -> x
+    | Delayed (s, f) ->
+        let x = f s in
+          t.s <- None;
+          t.v <- Value x;
+          x
+end
+
+module Keep_packed =
+struct
+  include Fast_write
+
+  let force t = match t.v with
+    | Value x -> x
+    | Delayed (s, f) -> f s
+end
+
+module Keep_compressed(C : sig
+    val compress : Reader.String_reader.t -> Reader.String_reader.t
+    val decompress : Reader.String_reader.t -> Reader.String_reader.t
+  end) =
+struct
+  include Fast_write
+
+  let from_val v = { s = None; v = Value v }
+
+  let from_fun s f =
+    let s = C.compress s in
+
+    let f s =
+      let s = C.decompress s in
+        f s
+    in
+      { s = Some s; v = Delayed (s, f); }
+
+  let force t = match t.v with
+    | Value x -> x
+    | Delayed (s, f) -> f s
+
+  let get_reader t = match t.v with
+    | Value _ -> None
+    | Delayed (s, _) -> Some (C.decompress s)
+end
+
+include Fast_write
---
+++ runtime/field.mli
@@ -0,0 +1,43 @@
+
+module type S =
+sig
+  type 'a t
+
+  val from_val : 'a -> 'a t
+  val from_fun :
+    Reader.String_reader.t -> (Reader.String_reader.t -> 'a) -> 'a t
+
+  val is_val     : 'a t -> bool
+  val force      : 'a t -> 'a
+  val get_reader : 'a t -> Reader.String_reader.t option
+end
+
+(** Field implementation that retains serialized data to support fast
+ * serialization. *)
+module Fast_write   : S
+
+(** Field implementation that discards serialized data when the value is
+ * forced. This reduces memory usage (since the underlying buffer can be
+ * released) but will make serialization slower.
+ * *)
+module Discard_data : S
+
+(** Field implementation that keeps data in "packed" (serialized) form.
+ * This minimizes memory usage, but makes both access and serialization
+ * slower.
+ * *)
+module Keep_packed  : S
+
+(** Field implementation that keeps data in compressed (serialized and then
+ * compressed) form.
+ * This minimizes memory usage, but makes both access and serialization
+ * slower.
+ * *)
+module Keep_compressed :
+  functor (C : sig
+    val compress   : Reader.String_reader.t -> Reader.String_reader.t
+    val decompress : Reader.String_reader.t -> Reader.String_reader.t
+  end) -> S
+
+(** The functionality in this module defaults to {!Fast_write}. *)
+include S
