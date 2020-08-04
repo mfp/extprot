ef2c6711bd5b89988a1958b288fc4f02d9348f5e
Mauricio Fernandez <mfp@acm.org>
2020-08-04 23:54:06 +0200

    Add trivial test case for ppx extension.

---
+++ test/testppx.ml
@@ -0,0 +1,10 @@
+type%extprot foo = { a : int; b : float }
+type%extprot bar = int
+type%extprot baz = { a : bar; b : int * int }
+type%extprot t1 = Int64.t
+type%extprot t2 = int
+type%extprot t3 = int list
+type%extprot t4 = float array
+type%extprot t5 = int * int * float array
+type%extprot ('a, 'b) t6 = int * 'a * 'b array
+type%extprot t7 = { a : int; b : (bool, Int64.t) t6 }
