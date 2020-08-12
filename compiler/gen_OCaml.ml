mfp/extprot

commit 9726d6db712ed5f8c0174a31c07dd9c2d58e5470

Preserve label attributes using name mangling + extra rewriting pass.

compiler/gen_OCaml.ml
@@ -505,8 +505,11 @@ let generate_container bindings =
             type_equals ~params ty (String.strip tyname)
           with exn -> bad_option ~msg:(Printexc.to_string exn) "type_equals" tyname in
 
+  let keep_ppx_mangled_name l =
+    List.filter (fun (k, v) -> k = "_ppx.mangled_name") l in
+
   let message_typedefs ~opts name ctyp =
-    let internal = typedef ~opts:[] ("_" ^ name) @@ maybe_type_equals opts ~params:[] ctyp in
+    let internal = typedef ~opts:(keep_ppx_mangled_name opts) ("_" ^ name) @@ maybe_type_equals opts ~params:[] ctyp in
     let ext      = typedef ~opts name @@ maybe_type_equals opts ~params:[] <:ctyp< $ctyp$ >> in
       <:str_item< $internal$; $ext$ >> in
 
@@ -520,11 +523,15 @@ let generate_container bindings =
             | _ -> failwith "low level msg def for `Message_record is not Message_single"
         in
 
-        let ctyp ((name, mutabl, ev_regime, _, texpr), llty) =
+        let ctyp ((name, mutabl, ev_regime, fopts, texpr), llty) =
           let ty = ctyp_of_texpr bindings texpr in
           let ty = match compute_ev_regime_with_llty llty ev_regime with
             | `Eager -> ty
-            | `Lazy -> <:ctyp< EXTPROT_FIELD____.t $ty$ >>
+            | `Lazy -> <:ctyp< EXTPROT_FIELD____.t $ty$ >> in
+
+          let name = match List.assoc "_ppx.mangled_name" fopts with
+            | exception Not_found -> name
+            | s -> s
           in
             match mutabl with
                 true -> <:ctyp< $lid:name$ : mutable $ty$ >>
@@ -795,11 +802,15 @@ let generate_container bindings =
     | Type_decl (name, params, texpr, opts) ->
         let ty = match poly_beta_reduce_texpr bindings texpr with
           | `Record (r, _) -> begin
-              let ctyp (name, mutabl, ev_regime, _opts, texpr) =
+              let ctyp (name, mutabl, ev_regime, fopts, texpr) =
                 let ty = ctyp_of_poly_texpr_core bindings texpr in
                 let ty = match ev_regime with
                   | `Eager | `Auto -> ty
-                  | `Lazy -> <:ctyp< EXTPROT_FIELD____.t $ty$ >>
+                  | `Lazy -> <:ctyp< EXTPROT_FIELD____.t $ty$ >> in
+
+                let name = match List.assoc "_ppx.mangled_name" fopts with
+                  | exception Not_found -> name
+                  | s -> s
                 in
                   match mutabl with
                     true -> <:ctyp< $lid:name$ : mutable $ty$ >>

compiler/ppx_extprot.ml
@@ -180,6 +180,89 @@ let () =
   Hashtbl.add extprot_attrs "extprot.pp" ();
   Hashtbl.add extprot_attrs "pp" ()
 
+type mangled_name_with_attrs = { name  : string; attrs : attributes; }
+
+let hex_alphabet = "0123456789abcdef"
+
+let to_hex s =
+  let b = Buffer.create (2 * String.length s) in
+    for i = 0 to String.length s - 1 do
+      Buffer.add_char b hex_alphabet.[Char.code s.[i] lsr 4];
+      Buffer.add_char b hex_alphabet.[Char.code s.[i] land 0xF];
+    done;
+    Buffer.contents b
+
+let hex_decode_char = function
+  | '0' .. '9' as c -> Char.code c - Char.code '0'
+  | 'a' .. 'f' as c -> Char.code c - Char.code 'a' + 10
+  | _   -> failwith "bad hex char"
+
+let from_hex s =
+  if String.length s land 1 <> 0 then failwith "bad hex string"
+  else
+    let b = Buffer.create (String.length s / 2) in
+    let i = ref 0 in
+      while !i < String.length s do
+        Buffer.add_char b
+          (Char.chr (hex_decode_char s.[!i] lsl 4 + hex_decode_char s.[!i+1]));
+        i := !i + 2
+      done;
+      Buffer.contents b
+
+let filter_out_extprot_attrs l =
+  List.filter
+    (fun { attr_name; _ } -> not @@ Hashtbl.mem extprot_attrs attr_name.txt) l
+
+let mangle_name_with_tydecl_attr_info t =
+  match filter_out_extprot_attrs @@ t.ptype_attributes with
+    | [] -> t.ptype_name.txt
+    | attrs ->
+        "__EXTPROT_" ^
+        (to_hex @@ Marshal.to_string { name = t.ptype_name.txt; attrs; } [])
+
+let unmangle_name s =
+  if String.length s < 10 then None
+  else
+    some @@
+    Marshal.from_string
+      (from_hex @@ String.sub s 10 (String.length s - 10)) 0
+
+class unmangle_names =
+  object(self)
+    inherit Ast_traverse.map
+
+    method! type_declaration t =
+      match unmangle_name t.ptype_name.txt with
+        | None ->
+            { t with ptype_kind = self#type_kind t.ptype_kind }
+        | Some { name; attrs } ->
+            { t with
+                ptype_name = { t.ptype_name with txt = name };
+                ptype_kind = self#type_kind t.ptype_kind;
+                ptype_attributes = attrs;
+            }
+
+    method! label_declaration t =
+      match unmangle_name t.pld_name.txt with
+        | None -> t
+        | Some { name; attrs } ->
+            { t with
+                pld_name = { t.pld_name with txt = name };
+                pld_attributes = attrs;
+            }
+  end
+
+let type_mangled_name_opt_of_tydecl t = match t.ptype_attributes with
+  | [] -> []
+  | _ -> [ "_ppx.mangled_name", mangle_name_with_tydecl_attr_info t ]
+
+let mangle_name_with_label_info t =
+  match filter_out_extprot_attrs @@ t.pld_attributes with
+    | [] -> t.pld_name.txt
+    | attrs ->
+        "__EXTPROT_" ^
+        (to_hex @@ Marshal.to_string { name = t.pld_name.txt; attrs; } [])
+
 let field_of_label_decl ?(autolazy = false) pld =
   let module Ast_builder = (val Ast_builder.make pld.pld_loc) in
   let open Ast_builder in
@@ -191,11 +274,15 @@ let field_of_label_decl ?(autolazy = false) pld =
       | _, Some _, None -> `Lazy
       | _, None, Some _ -> `Eager
       | true, None, None -> `Auto
-      | false, None, None -> `Eager
+      | false, None, None -> `Eager in
+
+  let fopts = match pld.pld_attributes with
+    | [] -> []
+    | _ -> [ "_ppx.mangled_name", mangle_name_with_label_info pld ]
   in
     (pld.pld_name.txt,
      (match pld.pld_mutable with Immutable -> false | Mutable -> true),
-     evr, [], tyexpr_of_core_type pld.pld_type)
+     evr, fopts, tyexpr_of_core_type pld.pld_type)
 
 let extract_type_param (ty, _) =
   match ty.ptyp_desc with
@@ -252,71 +339,6 @@ let pp_opt_of_tydecl t =
   match Attribute.get pp_attr t with
     | None -> []
     | Some e -> [ "ocaml.pp", string_of_expr e ]
-
-type mangled_name_with_attrs = { name  : string; attrs : attributes; }
-
-let hex_alphabet = "0123456789abcdef"
-
-let to_hex s =
-  let b = Buffer.create (2 * String.length s) in
-    for i = 0 to String.length s - 1 do
-      Buffer.add_char b hex_alphabet.[Char.code s.[i] lsr 4];
-      Buffer.add_char b hex_alphabet.[Char.code s.[i] land 0xF];
-    done;
-    Buffer.contents b
-
-let hex_decode_char = function
-  | '0' .. '9' as c -> Char.code c - Char.code '0'
-  | 'a' .. 'f' as c -> Char.code c - Char.code 'a' + 10
-  | _   -> failwith "bad hex char"
-
-let from_hex s =
-  if String.length s land 1 <> 0 then failwith "bad hex string"
-  else
-    let b = Buffer.create (String.length s / 2) in
-    let i = ref 0 in
-      while !i < String.length s do
-        Buffer.add_char b
-          (Char.chr (hex_decode_char s.[!i] lsl 4 + hex_decode_char s.[!i+1]));
-        i := !i + 2
-      done;
-      Buffer.contents b
-
-let filter_out_extprot_attrs l =
-  List.filter
-    (fun { attr_name; _ } -> not @@ Hashtbl.mem extprot_attrs attr_name.txt) l
-
-let mangle_name_with_tydecl_attr_info t =
-  match filter_out_extprot_attrs @@ t.ptype_attributes with
-    | [] -> t.ptype_name.txt
-    | attrs ->
-        "__EXTPROT_" ^
-        (to_hex @@ Marshal.to_string { name = t.ptype_name.txt; attrs; } [])
-
-let unmangle_name s =
-  if String.length s < 10 then None
-  else
-    some @@
-    Marshal.from_string
-      (from_hex @@ String.sub s 10 (String.length s - 10)) 0
-
-class unmangle_type_names =
-  object
-    inherit Ast_traverse.map
-
-    method! type_declaration t =
-      match unmangle_name t.ptype_name.txt with
-        | None -> t
-        | Some { name; attrs } ->
-            { t with
-                ptype_name = { t.ptype_name with txt = name };
-                ptype_attributes = attrs;
-            }
-  end
-
-let type_mangled_name_opt_of_tydecl t = match t.ptype_attributes with
-  | [] -> []
-  | _ -> [ "_ppx.mangled_name", mangle_name_with_tydecl_attr_info t ]
 
 (* force_message: set when using  extprot.message  to (1) reject
  * anything that is not a monomorphic record type and (2) register the
@@ -567,13 +589,13 @@ let register_decl_and_gencode (type a) (which : a gencode) ?(assume_subset = fal
           begin match Parse.implementation @@ Lexing.from_string implem with
             | _ :: str :: _ ->
                 (* skip the first element: EXTPROT_FIELD____ def *)
-                (new unmangle_type_names)#structure_item str
+                (new unmangle_names)#structure_item str
             | _ -> Location.raise_errorf ~loc "Codegen error"
           end
       | Gen_sig ->
           begin match Parse.interface @@ Lexing.from_string signature with
             | str :: _ ->
-                (new unmangle_type_names)#signature_item str
+                (new unmangle_names)#signature_item str
             | [] -> Location.raise_errorf ~loc "Codegen error"
           end
 

commit a43018be7d246fd1e6d7c79d476647396eb844d4

Fix bug introduced in 9726d6db71: prefix type names properly.
Needed to avoid duplicates in signatures.

compiler/gen_OCaml.ml
@@ -483,13 +483,13 @@ let get_mangled_name ~default opts =
 let generate_container bindings =
   let _loc = Loc.mk "gen_OCaml" in
 
-  let typedecl name ~opts ?(params = []) ctyp =
-    let name = get_mangled_name ~default:name opts in
+  let typedecl name ~prefix ~opts ?(params = []) ctyp =
+    let name = prefix ^ get_mangled_name ~default:name opts in
       Ast.TyDcl (_loc, name, params, ctyp, []) in
 
-  let typedef name ~opts ?(params = []) ctyp =
+  let typedef name ?(prefix="") ~opts ?(params = []) ctyp =
     let ctyp = get_type ctyp opts in
-    <:str_item< type $typedecl name ~opts ~params ctyp $ >> in
+    <:str_item< type $typedecl name ~prefix ~opts ~params ctyp $ >> in
 
   let type_equals ~params ty tyname =
     let applied =
@@ -511,7 +511,7 @@ let generate_container bindings =
     List.filter (fun (k, v) -> k = "_ppx.mangled_name") l in
 
   let message_typedefs ~opts name ctyp =
-    let internal = typedef ~opts:(keep_ppx_mangled_name opts) ("_" ^ name) @@ maybe_type_equals opts ~params:[] ctyp in
+    let internal = typedef ~prefix:"_" ~opts:(keep_ppx_mangled_name opts) name @@ maybe_type_equals opts ~params:[] ctyp in
     let ext      = typedef ~opts name @@ maybe_type_equals opts ~params:[] <:ctyp< $ctyp$ >> in
       <:str_item< $internal$; $ext$ >> in
 

compiler/ppx_extprot.ml
@@ -232,6 +232,14 @@ let unmangle_name s =
           some @@
           Marshal.from_string
             (from_hex @@ String.sub s 12 (String.length s - 12)) 0
+      | "___EXTPROT'_" when String.length s > 13 ->
+          (* for type names emitted with "_" prefix, the magic is
+           * "___EXTPROT'__" and we prepend the "_" prefix again *)
+          let name, attrs =
+            Marshal.from_string
+              (from_hex @@ String.sub s 13 (String.length s - 13)) 0
+          in
+            some { name = "_" ^ name; attrs; }
       | _ -> None
 
 class unmangle_names =